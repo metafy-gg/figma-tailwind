b21a90f653016bf2c26ed409e02900b3dfd49916
zakkor <edward.partenie@gmail.com>
2021-01-25 22:04:37 +0200

    Escape multiline string in a nicer way using awk

--- create-bookmarklet.sh
+++ create-bookmarklet.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-# Escape octal number sequences, remove all "`" characters, then wrap each line in `${LINE}\n`+ and finally remove the trailing "+"
-sed -e 's/\\[0-9][0-9]/\\\0/g' -e 's/`//g' -e 's/^/\`/' -e 'a \\\n\`+' tailwind.css | head -c -2 > escaped-multiline
+# Escape octal number sequences, remove all "`" characters, then wrap each line in '$LINE\n'+ and finally remove the trailing "+" using `head`
+sed -e 's/`//g' -e 's/\\[0-9][0-9]/\\\0/g' tailwind.css \
+  | awk 'BEGIN {ORS=""} { print "`" $0 "\\n`+" }' | head -c -1 > escaped-multiline
 
 # Replace the text marker with the contents of our escaped file
 sed '/<<REPLACE>>/{
